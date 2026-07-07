c3dd103dc449b38fba939449251088da43d5fd30
Fred HolahanCantwell <fredholahancantwell@Freds-MacBook-Air.local>
2026-07-07 12:28:02 +1000

    added main menu, first working version

--- task manager.py
+++ task manager.py
@@ -44,6 +44,29 @@ def remove_task():
     conn = get_connection()
     with conn.cursor() as cur:
         cur.execute(("DELETE FROM todos WHERE id = %s"), (removed_ID,))
-    
+
     conn.commit()
     conn.close()
+
+#=== menu ===
+while True:
+    print("\n1. Add task")
+    print("2. View tasks")
+    print("3. Complete task")
+    print("4. Remove task")
+    print("5. Quit")
+
+    choice = input("\nEnter a number: ")
+
+    if choice == "1":
+        add_task()
+    elif choice == "2":
+        view_tasks()
+    elif choice == "3":
+        complete_task()
+    elif choice == "4":
+        remove_task()
+    elif choice == "5":
+        break
+    else:
+        print("Invalid input, please enter a number between 1 and 5.")
