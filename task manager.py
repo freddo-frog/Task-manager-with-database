9f3e0c05eb2d08ae0e8eca6a6f1bc37bda9fb9f8
Fred HolahanCantwell <fredholahancantwell@Freds-MacBook-Air.local>
2026-07-07 11:49:10 +1000

    added all functions

---
+++ task manager.py
@@ -0,0 +1,49 @@
+#=== DATA ===
+import psycopg
+
+DB_NAME = "task_manager"
+
+#=== functions ===
+def get_connection():
+    return psycopg.connect(f"dbname={DB_NAME}")
+
+def view_tasks():
+    conn = get_connection()
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM todos;")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    conn.close()
+
+def add_task():
+    task = input("task name: ")
+    due_date = input("due date (dd/mm/yyyy): ")
+    created_at = input("time task was created? ")
+
+    conn = get_connection()
+    with conn.cursor() as cur:
+        cur.execute("INSERT INTO todos (task, due_date, created_at) VALUES(%s, %s, %s)", (task, due_date, created_at))
+
+    conn.commit()
+    conn.close()
+
+def complete_task():
+    task_ID = input("ID of completed task:")
+    conn = get_connection()
+
+    with conn.cursor() as cur:
+        cur.execute(("UPDATE todos SET completed = true WHERE id = %s"), (task_ID,)) #needs a trailing comma otherwise python does not register as a tuple
+    
+    conn.commit()
+    conn.close()
+
+def remove_task():
+    removed_ID = input("ID of the task you would like to remove?")
+
+    conn = get_connection()
+    with conn.cursor() as cur:
+        cur.execute(("DELETE FROM todos WHERE id = %s"), (removed_ID,))
+    
+    conn.commit()
+    conn.close()
